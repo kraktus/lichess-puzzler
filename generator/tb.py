88533a3465248cbf4b280c93dfe7384b64e87856
kraktus <kraktus@users.noreply.github.com>
2025-02-21 12:20:50 +0100

    remove debug

--- generator/tb.py
+++ generator/tb.py
@@ -52,16 +52,6 @@ class TbChecker:
         except requests.exceptions.RequestException as e:
             self.log.warning(f"req error while checking move pair for {fen}: {e}")
             return None
-        # DEBUG
-        import json, re
-        # using regex /b/b replace null -> None, true -> True, false -> False
-        dump = json.dumps(rep, indent=2)
-        dump = re.sub(r'\bnull\b', 'None', dump)
-        dump = re.sub(r'\btrue\b', 'True', dump)
-        dump = re.sub(r'\bfalse\b', 'False', dump)
-
-        print("rep", dump)
-        # DEBUG
         is_valid = True
         if rep["category"] != "win":
             is_valid = False
@@ -74,58 +64,3 @@ class TbChecker:
                 self.log.debug(f"in position {fen}, {move['uci']}({move['san']}) is not winning, opponent's category: {move['category']}")
                 is_valid = False
         return is_valid
-
-
-
-    # def check_winning(self: PuzzleChecker, fen: str, expected_move: str, rep: Dict[str, Any]) -> Set[Error]:
-    #     res = set()
-    #     if rep["category"] != "win":
-    #         log.error(f"position {fen} can't be won by side to move, category: " + "{}".format(rep["category"]))
-    #         res.add(Error.Wrong)
-    #     for move in rep["moves"]:
-    #         # move["category"] is from the opponent's point of vue
-    #         if move["uci"] == expected_move and move["category"] != "loss":
-    #             log.error(f"in position {fen}," + " {}({}) is not winning, opponent's category: {}".format(move["uci"], move["san"], move["category"]))
-    #             res.add(Error.Wrong)
-    #         elif move["category"] == "loss" and move["uci"] != expected_move: # a winning move which is not `expected_move`, puzzle is wrong 
-    #             log.error(f"in position {fen}," + " {}({}) is also winning".format(move["uci"], move["san"]))
-    #             res.add(Error.Multiple)
-    #     return res
-
-    # def check_drawing(self: PuzzleChecker, fen: str, expected_move: str, rep: Dict[str, Any]) -> Set[Error]:
-    #     res = set()
-    #     if not is_draw(rep):
-    #         log.error(f"position {fen} is not draw, category: " + "{}".format(rep["category"] ))
-    #         res.add(Error.Wrong)
-    #     for move in rep["moves"]:
-    #         # move["category"] is from the opponent's point of vue
-    #         if move["uci"] == expected_move and not is_draw(move):
-    #             log.error(f"in position {fen}," + " {}({}) is not drawing, opponent's category: {}".format(move["uci"], move["san"], move["category"]))
-    #             res.add(Error.Wrong)
-    #         elif is_draw(move) and move["uci"] != expected_move: # a drawing move which is not `expected_move`, puzzle is wrong 
-    #             log.error(f"in position {fen}," + " {}({}) is also drawing".format(move["uci"], move["san"]))
-    #             res.add(Error.Multiple)
-    #     return res
-
-    # def check_mate(self: PuzzleChecker, fen: str, expected_move: str, rep: Dict[str, Any], mate_in: int) -> Set[Error]:
-    #     res = set()
-    #     if rep["category"] != "win":
-    #         log.error(f"position {fen} can't be won by side to move, category: " + "{}".format(rep["category"]))
-    #         res.add(Error.Wrong)
-    #     if rep["dtm"] is not None and rep["dtm"] != mate_in:
-    #         log.error("position {} is not mate in {}, but {}.".format(fen, mate_in, rep["dtm"]))
-    #         res.add(Error.Wrong)
-    #     for move in rep["moves"]:
-    #         # move["category"] is from the opponent's point of vue
-    #         if move["checkmate"]: # Always good
-    #             continue
-    #         if move["uci"] == expected_move:
-    #             if move["category"] != "loss":
-    #                 log.error(f"in position {fen}," + " {}({}) is not winning, opponent's category: {}".format(move["uci"], move["san"], move["category"]))
-    #                 res.add(Error.Wrong)
-    #         elif move["dtm"] is not None:
-    #             # Another move that result in a mate in the same number of moves
-    #             if move["category"] == "loss" and -move["dtm"] == (mate_in - 1) : # DTM is negative since from the opponent point of view
-    #                 log.error("position {} after {} is not mate in {}, but {}.".format(fen, move["uci"], mate_in - 1, -move["dtm"]))
-    #                 res.add(Error.Multiple)
-    #         # Not checking puzzles without DTM because it is not possible to reliably convert DTZ to DTM.
